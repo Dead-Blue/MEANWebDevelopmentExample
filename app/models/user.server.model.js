06c4f719b14dd09506d9cc6358895c1af0c4d400
dead-blue <406354451@qq.com>
2015-10-17 10:58:28 +0800

    MEAN 0.5.0

---
+++ app/models/user.server.model.js
@@ -0,0 +1,32 @@
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+var UserSchema = new Schema({
+	firstName: String,
+	lastName: String,
+	email: String,
+	username: {
+		type:String,
+		trim: true
+	},
+	password: String,
+	created: {
+		type: Date,
+		default: Date.now
+	},
+	website: {
+		type: String,
+		get: function(url) {
+			if(!url) {
+				return url;
+			} else {
+				if(url.indexOf('http://')!==0 && url.indexOf('https://')!==0) {
+					url = 'http://' +url;
+				}
+				return url;
+			}
+		}
+	}
+});
+
+UserSchema.set('toJSON',{getters:true});
+mongoose.model('User', UserSchema);
