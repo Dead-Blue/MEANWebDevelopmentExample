b5dcf4eea7f8dab3bdf4be42ada966093a384d7e
dead-blue <406354451@qq.com>
2015-10-28 13:32:21 +0800

    finish half of chapter 10

--- app/tests/article.server.model.test.js
+++ app/tests/article.server.model.test.js
@@ -1,4 +1,4 @@
-var app =require('../../server/js');
+var app =require('../../server');
 var should = require('should');
 var mongoose = require('mongoose');
 var User = mongoose.model('User');
@@ -11,38 +11,38 @@ describe('Article Model Unit Tests:', function() {
 		user = new User({
 			firstName: 'Full',
 			lastName: 'Name',
-			displatName: 'Full Name',
+			displayName: 'Full Name',
 			email: 'test@test.com',
 			username: 'username',
 			password: 'password'
 		});
-		
-		uesr.save(function() {
+
+		user.save(function() {
 			article = new Article({
 				title: 'Article Title',
 				content: 'Article Content',
 				user: user
 			});
-			
+
 			done();
 		});
 	});
-	
+
 	describe('Testing the save method', function() {
 		it('Should be able to save without problems', function() {
 			article.save(function(err) {
 				should.not.exist(err);
 			});
 		});
-		
-		it('Should not be able to save an article withour a title', function() {
-			article = '';
+
+		it('Should not be able to save an article without a title', function() {
+			article.title = '';
 			article.save(function(err) {
 				should.exist(err);
 			});
 		});
 	});
-	
+
 	afterEach(function(done) {
 		Article.remove(function() {
 			User.remove(function() {
