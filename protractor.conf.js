8b62025258e34385b1e9943dbd3275f060b01cea
dead-blue <406354451@qq.com>
2015-10-29 12:30:12 +0800

    finish chapter10

---
+++ protractor.conf.js
@@ -0,0 +1,4 @@
+exports.config = {
+  directConnect:true,
+  specs: ['public/*[!lib]*/tests/e2e/*.js']
+}
---
+++ public/articles/tests/unit/articles.client.controller.unit.tests.js
@@ -0,0 +1,59 @@
+describe('Testing Articles controller', function() {
+  var _scope, ArticlesController;
+
+  beforeEach(function() {
+    module('mean');
+
+    jasmine.addMatchers({
+      toEqualData: function(util, customEqualityTesters) {
+        return {
+          compare: function(actual, expected) {
+            return {
+              pass: angular.equals(actual, expected)
+            };
+          }
+        };
+      }
+    });
+
+    inject(function($rootScope, $controller) {
+      _scope = $rootScope.$new();
+      ArticlesController = $controller('ArticlesController', {
+        $scope: _scope
+      });
+    });
+  });
+  it('Should have a find method that use $resource to retrive a list of articles', inject(function(Articles) {
+    inject(function($httpBackend) {
+      var sampleArticle = new Articles({
+        title: 'An Article about MEAN',
+        content: 'MEAN rocks!'
+      });
+      var sampleArticles = [sampleArticle];
+
+      $httpBackend.expectGET('api/articles').respond(sampleArticles);
+      _scope.find();
+      $httpBackend.flush();
+
+      expect(_scope.articles).toEqualData(sampleArticles);
+    });
+  }));
+
+  it('Should have a findOne method that use $resource to retreive a single of article', inject(function(Articles) {
+    inject (function($httpBackend, $routeParams) {
+      var sampleArticle = new Articles({
+        title: 'An Article about MEAN',
+        content: 'MEAN rocks!'
+      });
+
+      $routeParams.articleId = 'abcdef123456789012345678';
+
+      $httpBackend.expectGET(/api\/articles\/([0-9a-fA-F]{24})$/).respond (sampleArticle);
+
+      _scope.findOne();
+      $httpBackend.flush();
+
+      expect(_scope.article).toEqualData(sampleArticle);
+    });
+  }));
+});
